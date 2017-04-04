e6a66c4ce02709d095e89dd4b3580de0c20bbc86
Kelwinfm <kelwinfmendes@gmail.com>
2017-04-03 23:44:59 -0300

    Update a02ex03_g.hpp

--- a02ex03_g.hpp
+++ a02ex03_g.hpp
@@ -1,23 +1,17 @@
-/* Unicamp - Universidade Estadual de Campinas
-   FT - Faculdade de Tecnologia
-   Limeira - SP
-   Prof. Dr. Andre F. de Angelis
-   Maio/2015
-*/
-
 #ifndef A02EX03_G_H
    #define A02EX03_G_H
 
 #include <string>
 #include "a02ex03_b.hpp"
+#include "a02ex03_f.hpp"
 
 
-class CottageCheese : public Cheese
+class CottageCheese : public Food
 	{
-	private:	
+	private:
 	string tipo;
-	float peso;	
-	
+	float peso;
+
 	public:
 	CottageCheese(string, float, double);
 	virtual string getDescricao();
@@ -25,5 +19,5 @@ class CottageCheese : public Cheese
 
 
 #endif
-   
+
 /* fim de arquivo */
