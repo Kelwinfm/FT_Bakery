b1f77706d061569d4627ebc9d546ef93403513cc
Kelwinfm <kelwinfmendes@gmail.com>
2017-04-04 21:16:31 -0300

    Update a02ex03.hpp

--- a02ex03.hpp
+++ a02ex03.hpp
@@ -10,19 +10,49 @@ using namespace std;
 #ifndef A02EX03_H
    #define A02EX03_H
 
-    void process(void);
-    void verifyArguments(int, char* []);
-    void clearAll(void);
 
-    void listItems(void);
-    void insertItems(void);
-    void insertBread(void);
-    void insertCracker(void);
-    void insertFilledWafer(void);
-    void insertCheese(void);
-    void insertCottageCheese(void);
-    void insertHam(void);
-    void insertMortadella(void);
+class MyProgram
+{
+
+    private:
+        MyBooleanClass * verboseMode       = NULL;
+        MyBooleanClass * shortMessageMode  = NULL;
+        vector<Food *> myMainList;
+
+    public:
+        void get.verboseMode();
+        void set.verboseMode();
+        void get.shortMessageMode();
+        void set.shortMessageMode();
+
+        void start()
+        {
+        int main(int argc, char* argv[])
+           {
+           myMainList.clear();
+           Information::wellcome("C++ Program " + string(argv[0]) + " running!", shortMessageMode->getStatus());
+           process();
+           Information::bye(shortMessageMode->getStatus());
+           clearAll();
+           };
+        }
+        void process(void);
+        void clearAll(void);
+
+        void listItems(void);
+        void insertItems(void);
+        void insertBread(void);
+        void insertCracker(void);
+        void insertFilledWafer(void);
+        void insertCheese(void);
+        void insertCottageCheese(void);
+        void insertHam(void);
+        void insertMortadella(void);
+
+
+};
+
+    void verifyArguments(int, char* []);
 
 
 #endif
